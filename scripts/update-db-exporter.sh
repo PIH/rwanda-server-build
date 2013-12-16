8776cce1735c854913865c3080ffab9b2ea882c8
lnball <eball@pih.org>
2013-12-16 17:02:45 -0500

    Added updater for the database export application

---
+++ scripts/update-db-exporter.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update database exporter package
+cd $HOME/Workspace/database-exporter
+git pull --rebase
+
+# Build the databaseexporter executables including jar file
+mvn clean package -DskipTests
+
+# Copy jar file to $HOME/staging/bin directory
+cp target/databaseexporter-*-jar-with-dependencies.jar $HOME/staging/bin/db-exporter.jar
